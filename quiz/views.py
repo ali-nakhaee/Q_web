212546fedd6dfd9bc0ed657bc068e0bd65fd8400
ali <ali.nakhaee.kooshe@gmail.com>
2024-02-16 19:49:20 +0330

    formset added

--- quiz/views.py
+++ quiz/views.py
@@ -16,20 +16,22 @@ def quiz(request):
     questions = []
     for question_id in range(1, question_num):
         question = Question.objects.get(id=question_id)
-        questions.append({'text': question.text})
+        questions.append({'text': question.text, 'true_answer': question.true_answer})
 
     QuestionFormSet = formset_factory(QuestionForm, extra=0)
-    formset = QuestionFormSet(initial=questions)
 
     evaluation = []
-    """if request.method != 'POST':
+    if request.method != 'POST':
         # No data submitted; create a blank Quiz.
+        formset = QuestionFormSet(initial=questions)
 
-    
     else:
         # POST data submitted; process data.
+        formset = QuestionFormSet(data=request.POST, initial=questions)
+        # user_answers = QuestionFormSet.cleaned_data[0]['user_answer']
+        evaluation.append('your answer is true...')
 
-        question = Question.objects.get(id=1)
+        '''question = Question.objects.get(id=1)
         if float(user_answer1) == question.answer:
             evaluation.append("Your answer is True.")
         else:
@@ -40,7 +42,7 @@ def quiz(request):
         if float(user_answer2) == question.answer:
             evaluation.append("Your answer is True.")
         else:
-            evaluation.append(f"Your answer is False. Correct answer is {question.answer}.")"""
+            evaluation.append(f"Your answer is False. Correct answer is {question.answer}.")'''
 
     context = {'formset': formset, 'evaluation': evaluation}
     return render(request, 'quiz/quiz.html', context)
