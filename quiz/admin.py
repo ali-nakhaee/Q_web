6ded05b7bd2ab1a38c5d077b09305b55ca0a49c8
ali <ali.nakhaee.kooshe@gmail.com>
2024-03-08 23:16:15 +0330

    changes in admin page

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,6 +3,25 @@ from django.contrib import admin
 from .models import Question, Quiz, QuestionAnswer, QuizAnswer
 
 admin.site.register(Question)
-admin.site.register(Quiz)
+# admin.site.register(Quiz)
 admin.site.register(QuestionAnswer)
-admin.site.register(QuizAnswer)
+# admin.site.register(QuizAnswer)
+
+
+@admin.register(QuizAnswer)
+class QuizAnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'quiz', 'answer_duration', 'percent']
+    list_display_links = ['id', 'user', 'quiz']
+    sortable_by = ['answer_duration', 'date_answered', 'percent', 'id']
+    list_filter = ['quiz']
+    fields = [('user', 'quiz'), 'percent']
+
+
+class QuizAnswerInline(admin.TabularInline):
+    model = QuizAnswer
+    extra = 1
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['id', 'designer', 'duration']
+    inlines = [QuizAnswerInline]
