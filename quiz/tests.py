a9766583df4c7604554af6b70825f5ad3d5ac585
ali <ali.nakhaee.kooshe@gmail.com>
2024-03-26 19:13:29 +0330

    test for QuestionAnswer model added

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,16 +1,28 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import Question
+from .models import Question, Quiz, QuestionAnswer, QuizAnswer
 
 User = get_user_model()
 
 
 class TestQuestionModel(TestCase):
     def setUp(self):
-        user = User.objects.create(username='ali', password='123', first_name='ali',
+        user1 = User.objects.create(username='ali', password='123', first_name='ali',
                                    last_name='na')
-        Question.objects.create(text='2+2=', owner=user, true_answer=4)
-
+        question1 = Question.objects.create(text='2+2=', owner=user1, true_answer=4)
+        quiz1 = Quiz.objects.create(title='quiz1', designer=user1, duration=1,
+                                    is_published=True, answer_published=True)
+        quiz1.questions.add(question1)
+        quiz_answer1 = QuizAnswer.objects.create(user=user1, quiz=quiz1, percent=100,
+                                                 answer_duration=20)
+        QuestionAnswer.objects.create(question=question1, quiz=quiz1, quiz_answer=quiz_answer1,
+                                        user_answer=4, is_answered=True, evaluation=True)
+        
+        
     def test_question_answer(self):
-        question = Question.objects.get(text='2+2=')
-        self.assertEqual(question.true_answer, 4)
+        question_answer = QuestionAnswer.objects.get(question__text='2+2=')
+        self.assertEqual(question_answer.user_answer, 4)
+
+    def test_question_edit(self):
+        pass
+
