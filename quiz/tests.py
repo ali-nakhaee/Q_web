87f22121f1c35809e0e26c5c90ed71a28bb14c38
ali <ali.nakhaee.kooshe@gmail.com>
2024-03-26 13:57:54 +0330

    test created for question model. migrations deleted and new migration files created. Teacher permissions should be added manually

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Question
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TestQuestionModel(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='ali', password='123', first_name='ali',
+                                   last_name='na')
+        Question.objects.create(text='2+2=', owner=user, true_answer=4)
+
+    def test_question_answer(self):
+        question = Question.objects.get(text='2+2=')
+        self.assertEqual(question.true_answer, 4)
